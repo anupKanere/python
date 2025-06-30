4a8f10c65a18c7b8bac86b25c888cd375450bdc4
Anup Kanere <kanereanup@gmail.com>
2025-06-30 21:04:55 +0530

    leetcode practice code
    594. Longest Harmonious Subsequence

--- codes/01test.py
+++ codes/01test.py
@@ -1,8 +1,26 @@
-class Parent:
-    pass
+'''
+594. Longest Harmonious Subsequence
+'''
+from typing import List
+from collections import Counter
 
-class Child(Parent):
-    pass
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        max_length = 0
+        
+        print(counter)
+        
+        for num in counter:
+            if num + 1 in counter:
+                print(counter[num])
+                max_length = max(max_length, counter[num] + counter[num + 1])
 
-print(issubclass(Child, Parent))
-print(issubclass(Parent, Child))
+        return max_length
+
+
+sol = Solution()
+# nums = [1,3,2,2,5,2,3,7]
+nums = [1,2,3,4]
+result = sol.findLHS(nums)
+print(result)
